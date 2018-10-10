3da8f6603d07049e1b20f360a26c79ee9ba2b451
aa <lvze@tedu.cn>
2018-10-10 15:58:13 +0800

    第一次提交

---
+++ weibosearch/items.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Item, Field
+
+
+class WeiboItem(Item):
+    table_name = 'weibo'
+
+    id = Field()
+    content = Field()
+    forward_count = Field()
+    comment_count = Field()
+    like_count = Field()
+    posted_at = Field()
+    url = Field()
+    user = Field()
+    keyword=Field()
+
